72bb60c50b5f1f98d565078af3a4d97bad7530a7
yangjiang <jiangyang5157@gmail.com>
2019-06-02 17:49:05 +1200

    readme

--- mall/lib/src/pages/app/app_page.dart
+++ mall/lib/src/pages/app/app_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:mall/src/pages/app/app.dart';
 import 'package:mall/src/pages/home/home.dart';
 import 'package:mall/src/pages/splash/splash.dart';
 import 'package:mall/src/pages/login/login.dart';
-import 'package:mall/src/core/core.dart';
 
 class AppPage extends StatefulWidget {
   AppPage({Key key}) : super(key: key);
