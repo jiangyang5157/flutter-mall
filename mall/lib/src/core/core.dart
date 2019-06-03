c2931a238b873c2498d9e30a4491957e2c0fa2c6
yangjiang <jiangyang5157@gmail.com>
2019-06-03 18:24:38 +1200

    progress button2

--- mall/lib/src/core/core.dart
+++ mall/lib/src/core/core.dart
@@ -2,3 +2,4 @@ export 'package:mall/src/core/app_localization.dart';
 export 'package:mall/src/core/bloc_supervisor_delegate.dart';
 export 'package:mall/src/core/db_provider.dart';
 export 'package:mall/src/core/core_utils.dart';
+export 'package:mall/src/core/navigation.dart';
