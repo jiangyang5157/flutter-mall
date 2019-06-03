fb3a72c60a1f2485e4c3b8ed85e525c90d64a275
yangjiang <jiangyang5157@gmail.com>
2019-06-03 15:12:00 +1200

    na

--- mall/lib/src/core/core.dart
+++ mall/lib/src/core/core.dart
@@ -1,3 +1,4 @@
 export 'package:mall/src/core/app_localization.dart';
 export 'package:mall/src/core/bloc_supervisor_delegate.dart';
 export 'package:mall/src/core/db_provider.dart';
+export 'package:mall/src/core/core_utils.dart';
