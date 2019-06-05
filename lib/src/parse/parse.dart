9277646aecdcf075d6ab2bd382752440c8f44bf8
checkfree\yjiang <yang.jiang@fiserv.com>
2019-06-05 15:16:50 +1200

    rename

--- lib/src/parse/parse.dart
+++ lib/src/parse/parse.dart
@@ -1,4 +1,4 @@
 export 'package:mall/src/parse/user.dart';
-export 'package:mall/src/parse/user_repository_contract.dart';
-export 'package:mall/src/parse/user_repository.dart';
+export 'package:mall/src/parse/user_provider_contract.dart';
+export 'package:mall/src/parse/user_provider.dart';
 export 'package:mall/src/parse/parse_utils.dart';
