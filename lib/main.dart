5a16e872db840cdd20e600e826cac797da456f6b
yangjiang <jiangyang5157@gmail.com>
2019-06-06 00:59:21 +1200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:bloc/bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:mall/src/core/core.dart';
-import 'package:mall/src/pages/theme_app/theme_app.dart';
 
 void main() {
-  BlocSupervisor.delegate = BlocSupervisorDelegate();
-  runApp(ThemeAppPage());
+  runApp(AppPage());
+}
+
+class AppPage extends StatefulWidget {
+  AppPage({Key key}) : super(key: key);
+
+  @override
+  _AppPageState createState() => _AppPageState();
+}
+
+class _AppPageState extends State<AppPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        const AppLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizationsDelegate.supportedLanguageCodes
+          .map<Locale>((languageCode) => Locale(languageCode)),
+      onGenerateRoute: App().router.generator,
+      theme: App().themeData,
+    );
+  }
 }
