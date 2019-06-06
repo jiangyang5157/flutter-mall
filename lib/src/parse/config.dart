8b67ab683b3f3fb70e2f1464174e464ddf061791
yangjiang <jiangyang5157@gmail.com>
2019-06-07 00:59:00 +1200

    update server ip

--- lib/src/parse/config.dart
+++ lib/src/parse/config.dart
@@ -3,7 +3,7 @@ const String parseApplicationId = 'myAppId';
 const String parseMasterKey = 'myMasterKey';
 //const String parseServerUrl = 'http://localhost:1337/parse';
 //const String parseServerUrl = 'http://10.0.2.2:1337/parse'; // Android emulator localhost
-const String parseServerUrl = 'http://111.231.2.169:1337/parse';
+const String parseServerUrl = 'http://118.24.251.163:1337/parse';
 
 const String masterUserName = 'master';
 const String masterUserPassword = 'master';
