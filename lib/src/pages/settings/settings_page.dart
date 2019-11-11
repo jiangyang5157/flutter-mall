109e09c99fe6c3667d704bf73b513c21fa346532
Yang Jiang <jiangyang5157@gmail.com>
2019-11-11 15:25:47 +1300

    navigateTo settings

--- lib/src/pages/settings/settings_page.dart
+++ lib/src/pages/settings/settings_page.dart
@@ -33,17 +33,77 @@ class _SettingsPageState extends State<SettingsPage> {
 
     return ChangeNotifierProvider<UserModel>(
       builder: (context) => userModel,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(string(context, 'title_settings')),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[],
-          ),
-        ),
+      child: Consumer<UserModel>(
+        builder: (context, userModel, _) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(string(context, 'title_settings')),
+            ),
+            body: Center(
+              child: ListView(
+                padding: const EdgeInsets.all(0),
+                children: (_buildSettingsList(context, userModel)),
+              ),
+            ),
+          );
+        },
       ),
     );
+  }
+
+  List<Widget> _buildSettingsList(BuildContext context, UserModel userModel) {
+    var ret = List<Widget>();
+    ret.addAll(_buildProfileList(context, userModel));
+    ret.add(Divider());
+//    ret.add(ToggleButtons());
+    return ret;
+  }
+
+  List<Widget> _buildProfileList(BuildContext context, UserModel userModel) {
+    var ret = List<Widget>();
+    if (userModel.type == UserType.Anonymous) {
+      ret.add(
+        ListTile(
+          title: Text(string(context, 'label_create_account')),
+          onTap: () {
+            todo(context);
+          },
+        ),
+      );
+    } else {
+      ret.add(
+        ListTile(
+          title: Text(string(context, 'label_upload_display_picture')),
+          onTap: () {
+            todo(context);
+          },
+        ),
+      );
+      ret.add(
+        ListTile(
+          title: Text(string(context, 'label_change_username')),
+          onTap: () {
+            todo(context);
+          },
+        ),
+      );
+      ret.add(
+        ListTile(
+          title: Text(string(context, 'label_change_password')),
+          onTap: () {
+            todo(context);
+          },
+        ),
+      );
+      ret.add(
+        ListTile(
+          title: Text(string(context, 'label_update_email_address')),
+          onTap: () {
+            todo(context);
+          },
+        ),
+      );
+    }
+    return ret;
   }
 }
