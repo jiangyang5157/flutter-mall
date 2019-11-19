d9eb235dde7cac403490a277bf7ce0497b3e425d
Yang Jiang <jiangyang5157@gmail.com>
2019-11-19 15:27:15 +1300

    na

--- lib/features/theme/data/repositories/theme_repository_impl.dart
+++ lib/features/theme/data/repositories/theme_repository_impl.dart
@@ -2,46 +2,25 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:mall/core/error/exceptions.dart';
 import 'package:mall/core/error/failures.dart';
-import 'package:mall/core/network/network_info.dart';
 import 'package:mall/features/theme/data/models/theme_model.dart';
 import 'package:mall/features/theme/data/sources/theme_local_data_source.dart';
-import 'package:mall/features/theme/data/sources/theme_remote_data_source.dart';
 import 'package:mall/features/theme/domain/entities/theme_entity.dart';
 import 'package:mall/features/theme/domain/repositories/theme_repository.dart';
 
 class ThemeRepositoryImpl implements ThemeRepository {
   final ThemeLocalDataSource localDataSource;
-  final ThemeRemoteDataSource remoteDataSource;
-  final NetworkInfo networkInfo;
 
   ThemeRepositoryImpl({
     @required this.localDataSource,
-    @required this.remoteDataSource,
-    @required this.networkInfo,
   });
 
   @override
-  Future<bool> shouldFetch() {
-    return Future.value(false);
-  }
-
-  @override
   Future<Either<Failure, ThemeModel>> getTheme() async {
-    if (await shouldFetch() && await networkInfo.isConnected) {
-      try {
-        return Right(await remoteDataSource.fetchTheme().then((result) {
-          localDataSource.cacheTheme(result);
-          return result;
-        }));
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      try {
-        return Right(await localDataSource.getLastTheme());
-      } on CacheException {
-        return Left(CacheFailure());
-      }
+    try {
+      final ret = await localDataSource.getLastTheme();
+      return Right(ret);
+    } on CacheException {
+      return Left(CacheFailure());
     }
   }
 
--- lib/features/theme/domain/repositories/theme_repository.dart
+++ lib/features/theme/domain/repositories/theme_repository.dart
@@ -3,9 +3,6 @@ import 'package:mall/core/error/failures.dart';
 import 'package:mall/features/theme/domain/entities/theme_entity.dart';
 
 abstract class ThemeRepository {
-
-  Future<bool> shouldFetch();
-
   Future<Either<Failure, ThemeEntity>> getTheme();
 
   Future<Either<Failure, ThemeEntity>> saveTheme(ThemeType type);
