7c789f449e44502fa2ef73224fe440d2e7ff33c1
Yang Jiang <jiangyang5157@gmail.com>
2019-11-22 16:30:49 +1300

    na

--- lib/features/backend/data/sources/user_local_data_source.dart
+++ lib/features/backend/data/sources/user_local_data_source.dart
@@ -1,23 +1,26 @@
 import 'package:mall/core/error/exceptions.dart';
-import 'package:mall/features/backend/domain/entities/user_entity.dart';
+import 'package:mall/features/backend/data/models/user_model.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
 
 abstract class UserLocalDataSource {
   /// Throws [CacheException] if no cached data is present.
-  Future<UserEntity> getLastData();
+  Future<UserModel> getLastData();
 
-  Future<void> cacheData(UserEntity entity);
+  Future<void> cacheData(UserModel model);
 }
 
 class UserLocalDataSourceImpl implements UserLocalDataSource {
   @override
-  Future<void> cacheData(UserEntity entity) {
-    // TODO: implement cacheData
-    return null;
+  Future<void> cacheData(UserModel model) {
+    return model.user.save();
   }
 
   @override
-  Future<UserEntity> getLastData() {
-    // TODO: implement getLastData
-    return null;
+  Future<UserModel> getLastData() async {
+    ParseUser parseUser = await ParseUser.currentUser();
+    if (parseUser == null) {
+      throw CacheException();
+    }
+    return UserModel(user: parseUser);
   }
 }
--- lib/features/backend/data/sources/user_remote_data_source.dart
+++ lib/features/backend/data/sources/user_remote_data_source.dart
@@ -1,14 +1,25 @@
-import 'package:mall/features/backend/domain/entities/user_entity.dart';
+import 'package:mall/core/error/exceptions.dart';
+import 'package:mall/features/backend/data/models/user_model.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
 
 abstract class UserRemoteDataSource {
   /// Throws a [ServerException] for all error codes.
-  Future<UserEntity> getLastData();
+  Future<UserModel> getLastData();
 }
 
-class UserLocalDataSourceImpl implements UserRemoteDataSource {
+class UserRemoteDataSourceImpl implements UserRemoteDataSource {
   @override
-  Future<UserEntity> getLastData() {
-    // TODO: implement getLastData
-    return null;
+  Future<UserModel> getLastData() async {
+    ParseResponse result = await ParseUser.getCurrentUserFromServer();
+    if (result == null) {
+      print('#### current user not found from server.');
+      throw ServerException();
+    }
+
+    if (!result.success) {
+      print('#### current user not found from server: ${result.error.message}');
+      throw ServerException();
+    }
+    return UserModel(user: result.result);
   }
 }
